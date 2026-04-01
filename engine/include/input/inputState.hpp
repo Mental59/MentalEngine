1bcf9f84a21e3c52ad3bd26163fbdae5d5d56b54
Mental <Alekov99@mail.ru>
2026-04-01 23:32:22 +0500

    Move input state implementation to source file

--- engine/include/input/inputState.hpp
+++ engine/include/input/inputState.hpp
@@ -7,82 +7,30 @@ namespace mental::input
 class InputState
 {
  public:
-  void advance(const InputSnapshot& snapshot) noexcept
-  {
-    mPreviousRawSnapshot = mCurrentRawSnapshot;
-    mCurrentRawSnapshot = snapshot;
+  void advance(const InputSnapshot& snapshot) noexcept;
 
-    if (mHasCurrentCursorPosition)
-    {
-      mPreviousCursorPosition = mCurrentCursorPosition;
-    }
-    else
-    {
-      mPreviousCursorPosition = snapshot.cursorPosition;
-      mHasCurrentCursorPosition = true;
-    }
+  [[nodiscard]] bool isKeyDown(KeyCode keyCode) const noexcept;
 
-    mCurrentCursorPosition = snapshot.cursorPosition;
-  }
+  [[nodiscard]] bool wasKeyPressed(KeyCode keyCode) const noexcept;
 
-  [[nodiscard]] bool isKeyDown(KeyCode keyCode) const noexcept
-  {
-    return mCurrentRawSnapshot.isKeyDown(keyCode);
-  }
+  [[nodiscard]] bool wasKeyReleased(KeyCode keyCode) const noexcept;
 
-  [[nodiscard]] bool wasKeyPressed(KeyCode keyCode) const noexcept
-  {
-    return mCurrentRawSnapshot.isKeyDown(keyCode) && !mPreviousRawSnapshot.isKeyDown(keyCode);
-  }
+  [[nodiscard]] bool isMouseButtonDown(MouseButton mouseButton) const noexcept;
 
-  [[nodiscard]] bool wasKeyReleased(KeyCode keyCode) const noexcept
-  {
-    return !mCurrentRawSnapshot.isKeyDown(keyCode) && mPreviousRawSnapshot.isKeyDown(keyCode);
-  }
+  [[nodiscard]] bool wasMouseButtonPressed(MouseButton mouseButton) const noexcept;
 
-  [[nodiscard]] bool isMouseButtonDown(MouseButton mouseButton) const noexcept
-  {
-    return mCurrentRawSnapshot.isMouseButtonDown(mouseButton);
-  }
+  [[nodiscard]] bool wasMouseButtonReleased(MouseButton mouseButton) const noexcept;
 
-  [[nodiscard]] bool wasMouseButtonPressed(MouseButton mouseButton) const noexcept
-  {
-    return mCurrentRawSnapshot.isMouseButtonDown(mouseButton) && !mPreviousRawSnapshot.isMouseButtonDown(mouseButton);
-  }
+  [[nodiscard]] CursorPosition mousePosition() const noexcept;
 
-  [[nodiscard]] bool wasMouseButtonReleased(MouseButton mouseButton) const noexcept
-  {
-    return !mCurrentRawSnapshot.isMouseButtonDown(mouseButton) && mPreviousRawSnapshot.isMouseButtonDown(mouseButton);
-  }
+  [[nodiscard]] CursorPosition mouseDelta() const noexcept;
 
-  [[nodiscard]] CursorPosition mousePosition() const noexcept
-  {
-    return mCurrentCursorPosition;
-  }
-
-  [[nodiscard]] CursorPosition mouseDelta() const noexcept
-  {
-    return {
-      .x = mCurrentCursorPosition.x - mPreviousCursorPosition.x,
-      .y = mCurrentCursorPosition.y - mPreviousCursorPosition.y,
-    };
-  }
-
-  [[nodiscard]] ScrollDelta scrollDelta() const noexcept
-  {
-    return mCurrentRawSnapshot.scrollDelta;
-  }
+  [[nodiscard]] ScrollDelta scrollDelta() const noexcept;
 
  protected:
-  [[nodiscard]] const InputSnapshot& currentRawSnapshot() const noexcept
-  {
-    return mCurrentRawSnapshot;
-  }
+  [[nodiscard]] const InputSnapshot& currentRawSnapshot() const noexcept;
 
-  [[nodiscard]] const InputSnapshot& previousRawSnapshot() const noexcept
-  {
-    return mPreviousRawSnapshot;
-  }
+  [[nodiscard]] const InputSnapshot& previousRawSnapshot() const noexcept;
 
  private:
   InputSnapshot mPreviousRawSnapshot {};
