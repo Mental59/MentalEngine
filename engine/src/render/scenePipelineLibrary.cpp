155bb61bc48f7e4e1a884e420559ea887e13d4ec
Mental <Alekov99@mail.ru>
2026-04-09 12:37:14 +0500

    Disabled culling

--- engine/src/render/scenePipelineLibrary.cpp
+++ engine/src/render/scenePipelineLibrary.cpp
@@ -387,7 +387,7 @@ mental::core::Result mental::render::ScenePipelineLibrary::createPipelines()
     .pushConstantRangeCount = 1u,
     .topology = rhi::PrimitiveTopology::eTriangleList,
     .polygonMode = rhi::PolygonMode::eFill,
-    .cullMode = rhi::CullMode::eBack,
+    .cullMode = rhi::CullMode::eNone,
     .frontFace = rhi::FrontFace::eCounterClockwise,
     .depthTestEnable = true,
     .depthWriteEnable = true,
